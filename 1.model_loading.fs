309f44a66375a91e8607f63ece2201168adb0821
Ryan Plyler <g.r.plyler@gmail.com>
2024-12-09 20:34:53 -0800

    depth testing

--- 1.model_loading.fs
+++ 1.model_loading.fs
@@ -5,7 +5,17 @@ in vec2 TexCoords;
 
 uniform sampler2D texture_diffuse1;
 
+float near = 0.1; 
+float far  = 5.0;
+
+float LinDepth(float depth) {
+    float z = depth * 2.0 - 1.0; // Back to NDC
+    return (2.0 * near * far) / (far + near - z * (far - near));
+}
+
 void main()
+
 {    
-    FragColor = texture(texture_diffuse1, TexCoords);
+    float depth = LinDepth(gl_FragCoord.z) / far;
+    FragColor = vec4(vec3(depth), 1.0);
 }
