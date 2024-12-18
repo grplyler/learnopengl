6d9cca3bc6bb2dabbc01c87958034e67cfddb888
Ryan Plyler <g.r.plyler@gmail.com>
2024-12-17 22:53:24 -0800

    green stuff

--- src/shaders/framebuffers_screen.fs
+++ src/shaders/framebuffers_screen.fs
@@ -4,9 +4,69 @@ out vec4 FragColor;
 in vec2 TexCoords;
 
 uniform sampler2D screenTexture;
+const float offset = 1.0 / 300.0;  
 
 void main()
 {
-    vec3 col = texture(screenTexture, TexCoords).rgb;
-    FragColor = vec4(col, 1.0);
+    // Invert the screen colors
+    // vec3 col = texture(screenTexture, TexCoords).rgb;
+    // FragColor = vec4(vec3(1.0 - col), 1.0);
+
+    // Grayscale the screen colors
+    // FragColor = texture(screenTexture, TexCoords);
+    // float average = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
+    // FragColor = vec4(average, average, average, 1.0);
+
+    // Natural greyscale
+    // FragColor = texture(screenTexture, TexCoords);
+    // float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
+    // FragColor = vec4(average, average, average, 1.0);
+
+    // Kernel
+    vec2 offsets[9] = vec2[](
+        vec2(-offset,  offset), // top-left
+        vec2( 0.0f,    offset), // top-center
+        vec2( offset,  offset), // top-right
+        vec2(-offset,  0.0f),   // center-left
+        vec2( 0.0f,    0.0f),   // center-center
+        vec2( offset,  0.0f),   // center-right
+        vec2(-offset, -offset), // bottom-left
+        vec2( 0.0f,   -offset), // bottom-center
+        vec2( offset, -offset)  // bottom-right    
+    );
+
+    // blur kernel
+    // float kernel[9] = float[](
+    //     1.0 / 16, 2.0 / 16, 1.0 / 16,
+    //     2.0 / 16, 4.0 / 16, 2.0 / 16,
+    //     1.0 / 16, 2.0 / 16, 1.0 / 16  
+    // );
+
+    // edge detect kernel
+    float kernel[9] = float[](
+        -1.0, -1.0, -1.0,
+        -1.0,  8.0, -1.0,
+        -1.0, -1.0, -1.0
+    );
+
+    // emboss
+    // float kernel[9] = float[](
+    //     -2.0, -1.0, 0.0,
+    //     -1.0,  1.0, 1.0,
+    //      0.0,  1.0, 2.0
+    // );
+    
+    vec3 sampleTex[9];
+    for(int i = 0; i < 9; i++)
+    {
+        sampleTex[i] = vec3(texture(screenTexture, TexCoords.st + offsets[i]));
+    }
+    vec3 col = vec3(0.0);
+    for(int i = 0; i < 9; i++)
+        col += sampleTex[i] * kernel[i];
+    
+    float average = (col.r + col.g + col.b) / 3.0;
+
+    FragColor = vec4(0.0, average, 0.0, 1.0);
+
 } 
